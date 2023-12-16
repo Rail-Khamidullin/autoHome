6bdd474d688d88787e84831cfdc85fb724293da8
Rail <74302526+Rail-Khamidullin@users.noreply.github.com>
2023-12-16 18:44:49 +0400

    add comment in gitHomework

--- src/test/java/github/gitHomework.java
+++ src/test/java/github/gitHomework.java
@@ -12,12 +12,14 @@ public class gitHomework {
     void gitHubWork() {
         //    Открытие страницы по абсолютному url
         open("https://github.com/");
+        // Находим в хедере текст Solutions и подводим к нему мышь
         $(byText("Solutions")).hover();
-
+        // Берём первый элемент из выпадающего списка и кликаем его
         $(".border-bottom").$$("ul li").first().click();
+        // Находим текст на новой странице developer platform. и подводим к нему мышь
         $(byText("developer platform.")).hover();
 
-
+        // Тайм аут в 5 секунд, чтобы посмотреть результат
         sleep(5000);
     }
 }
